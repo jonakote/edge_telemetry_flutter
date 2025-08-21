a8249762806fcd80c46ae802b8b3ba4003a35b1f
Joshua Tetteh <joshuatettehx@gmail.com>
2025-08-21 09:38:46 +0000

    feat: conditionally collect platform information for non-web devices

--- lib/src/collectors/flutter_device_info_collector.dart
+++ lib/src/collectors/flutter_device_info_collector.dart
@@ -40,7 +40,7 @@ class FlutterDeviceInfoCollector implements DeviceInfoCollector {
       });
 
       // Collect platform information
-      attributes['device.platform'] = Platform.operatingSystem;
+      attributes['device.platform'] = kIsWeb ? "web" : Platform.operatingSystem;
       attributes['device.platform_version'] = Platform.operatingSystemVersion;
 
       // Collect platform-specific device information
